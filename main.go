399e706dcfaecf3e08341755658aba56d09fcdcc
XiaoJie Guo <guoxiaojie@onchain.com>
2017-03-20 17:05:50 +0800

    Support TLS for email
    1.Support TLS for email
    2.Add a enableTLS configuration in mailpatch.json
    
    Signed-off-by: XiaoJie Guo <guoxiaojie@onchain.com>

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/smtp"
 	"os"
@@ -26,6 +28,74 @@ var emailSender string
 var senderPasswd string
 var smtpServerAddr string
 var emailReceivers string
+var enableTLS bool
+
+// sendEmailUseTLS ---
+func sendEmailUseTLS(sender, password, smtpServer, to, subject, emailBody, mailType string) error {
+	senderName := strings.Split(sender, "@")
+
+	//create smtp client
+	conn, err := tls.Dial("tcp", smtpServer, nil)
+	if err != nil {
+		log.Println("Dialing Error:", err)
+		return err
+	}
+
+	smtpSrv := strings.Split(smtpServer, ":")
+	c, err := smtp.NewClient(conn, smtpSrv[0])
+	if err != nil {
+		log.Println("Create smpt client error:", err)
+		return err
+	}
+	defer c.Close()
+
+	auth := smtp.PlainAuth("", sender, password, smtpSrv[0])
+	if auth != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(auth); err != nil {
+				log.Println("Error during AUTH", err)
+				return err
+			}
+		}
+	}
+
+	if err = c.Mail(sender); err != nil {
+		return err
+	}
+
+	recepter := strings.Split(to, ";")
+	for _, addr := range recepter {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	var contentType string
+	if mailType == "html" {
+		contentType = "Content-Type: text/" + mailType + "; charset=UTF-8"
+	} else {
+		contentType = "Content-Type: text/plain" + "; charset=UTF-8"
+	}
+	email := []byte("To: " + to + "\r\nFrom: " + senderName[0] + "<" + sender + ">\r\nSubject: " +
+		subject + "\r\n" + contentType + "\r\n\r\n" + emailBody)
+
+	_, err = w.Write(email)
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return c.Quit()
+}
 
 // sendEmail ---
 func sendEmail(sender, password, smtpServer, to, subject, emailBody, mailType string) error {
@@ -102,9 +172,20 @@ func mailPatch(w http.ResponseWriter, req *http.Request) {
 		fmt.Println(err)
 	}
 
-	//sendEmail of patch to
-	err = sendEmail(emailSender, senderPasswd, smtpServerAddr, emailReceivers,
-		emailSubject, patchContent, "txt")
+	//add the commiter email addr to the receivers
+	commiterHead := strings.Split(patchContent, ">")
+	commiter := strings.Split(commiterHead[0], "<")
+	emailReceivers += ";"
+	emailReceivers += commiter[1]
+
+	//send email
+	if enableTLS {
+		err = sendEmailUseTLS(emailSender, senderPasswd, smtpServerAddr, emailReceivers,
+			emailSubject, patchContent, "txt")
+	} else {
+		err = sendEmail(emailSender, senderPasswd, smtpServerAddr, emailReceivers,
+			emailSubject, patchContent, "txt")
+	}
 	if err != nil {
 		fmt.Println("SendEmail error!")
 		fmt.Println(err)
@@ -135,32 +216,39 @@ func loadConf(path string) error {
 	if 0 == len(localServer) {
 		return errors.New("load config localServer error: ")
 	}
-	fmt.Println(localServer)
+	fmt.Println("localServer:\t", localServer)
 	emailSubject = js.Get("emailSubject").MustString()
 	if 0 == len(emailSubject) {
 		return errors.New("load config emailSubject error: ")
 	}
-	fmt.Println(emailSubject)
+	fmt.Println("emailSubject:\t", emailSubject)
 	emailSender = js.Get("emailSender").MustString()
 	if 0 == len(emailSender) {
 		return errors.New("load config emailSender error: ")
 	}
-	fmt.Println(emailSender)
+	fmt.Println("emailSender:\t", emailSender)
 	senderPasswd = js.Get("senderPasswd").MustString()
 	if 0 == len(senderPasswd) {
 		return errors.New("load config senderPasswd error: ")
 	}
-	fmt.Println(senderPasswd)
+	fmt.Println("senderPasswd:\t", senderPasswd)
 	smtpServerAddr = js.Get("smtpServerAddr").MustString()
 	if 0 == len(smtpServerAddr) {
 		return errors.New("load config smtpServerAddr error: ")
 	}
-	fmt.Println(smtpServerAddr)
+	fmt.Println("smtpServerAddr:\t", smtpServerAddr)
 	emailReceivers = js.Get("emailReceivers").MustString()
 	if 0 == len(emailReceivers) {
 		return errors.New("load config emailReceivers error: ")
 	}
-	fmt.Println(emailReceivers)
+	fmt.Println("emailReceivers:\t", emailReceivers)
+	enableTLS = js.Get("enableTLS").MustBool()
+	if enableTLS {
+		fmt.Println("TLS: ", "TLS enable")
+	} else {
+		fmt.Println("TLS: ", "TLS disable")
+	}
+
 	return nil
 }
 
@@ -168,6 +256,7 @@ func main() {
 	err := loadConf(MAILPATCHJSON)
 	if err != nil {
 		fmt.Println("LoadConf error: ", err.Error())
+		return
 	}
 
 	fmt.Println("MailPatch Server Start!")
